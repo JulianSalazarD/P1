ac70940a0561c5911b154dcc274eb7ddb3dd44e2
Julian Salazar <jusalazard@unal.edu.co>
2023-04-17 23:24:30 -0500

    Clase abstracta camion

--- SistemaGestionTransporte/src/gestorAplicaciones/Usuario.java
+++ SistemaGestionTransporte/src/gestorAplicaciones/Usuario.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Usuario{
 	
 	//atributos serializador
-	private static final long serialVersionUID = 1L;
+	//private static final long serialVersionUID = 1L;
 	private static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 	private final String nombre;
 	private String clave;
